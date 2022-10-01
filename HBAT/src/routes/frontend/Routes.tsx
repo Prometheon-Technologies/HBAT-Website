67ba9194c36a941a3cc34a30bf185c7e99ad5abf
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-10-01 17:38:35 +0100

    update -begin implementing gh-pages

--- HBAT/src/routes/frontend/Routes.tsx
+++ HBAT/src/routes/frontend/Routes.tsx
@@ -1,6 +1,6 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import AntxChatDesign01 from '../../pages/HbatDesign01';
-import AntxChatDesign02 from '../../pages/HbatDesign02';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AntxChatDesign01 from "../../pages/HbatDesign01";
+import AntxChatDesign02 from "../../pages/HbatDesign02";
 
 const AppRoutes = () => {
   return (
@@ -8,11 +8,10 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<AntxChatDesign01 />} />
         <Route path="/design-01" element={<AntxChatDesign01 />} />
-        <Route path="/white-papers" element={<AntxChatDesign02/>} />
-
+        <Route path="/white-papers" element={<AntxChatDesign02 />} />
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default AppRoutes;
