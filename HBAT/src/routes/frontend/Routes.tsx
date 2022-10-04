1ec031d761d8e4574798e274f130d152901ff07e
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-10-04 13:43:55 +0100

    update - fix routing issues

--- HBAT/src/routes/frontend/Routes.tsx
+++ HBAT/src/routes/frontend/Routes.tsx
@@ -1,17 +1,19 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import AntxChatDesign01 from '../../pages/HbatDesign01';
-import AntxChatDesign02 from '../../pages/HbatDesign02';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AntxChatDesign01 from "../../pages/HbatDesign01";
+import AntxChatDesign02 from "../../pages/HbatDesign02";
+
+var end_point: ImportMetaEnv = { PUBLIC_URL: "HBAT-Website" };
 
 const AppRoutes = () => {
   return (
-    <BrowserRouter basename="/HBAT-Website">
+    <BrowserRouter basename={`/${end_point.PUBLIC_URL}`}>
       <Routes>
-        <Route path="/" element={<AntxChatDesign01 />} />
+        <Route path="" element={<AntxChatDesign01 />} />
         <Route path="/home" element={<AntxChatDesign01 />} />
         <Route path="/white-papers" element={<AntxChatDesign02 />} />
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default AppRoutes;
